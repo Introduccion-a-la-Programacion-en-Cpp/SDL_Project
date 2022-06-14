3b47a60866678b5391454c872965b58eb9bf83b6
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-06-14 16:59:21 -0300

    added BGM and collections

--- src/SDL_Project.cpp
+++ src/SDL_Project.cpp
@@ -3,37 +3,111 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "SDL.h"
 #include "SDL_image.h"
 #include "SDL_TTF.h"
+#include "SDL_mixer.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
+///////// Definicion de estructuras /////////////
+typedef struct InputState {
+    bool up = false;
+    bool down = false;
+    bool right = false;
+    bool left = false;
+    bool fire = false;
+};
 
-    SDL_Init(SDL_INIT_VIDEO);
+typedef struct Sprite {
+    SDL_Texture* texture;
+    SDL_Rect dest;
+    bool isVisible = true;
+};
+
+typedef struct Text {
+    TTF_Font* font;
+    SDL_Color color;
+    SDL_Surface* surface;
+    SDL_Texture* texture;
+    SDL_Rect dest;
+    bool isVisible = true;
+};
+
+typedef struct Bgm {
+    Mix_Music* music;
+};
+
+///////// Definicion de estructuras /////////////
+
+///////// Variables y Constantes Globales /////////////
+const int WIDTH = 640;
+const int HEIGHT = 480;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_bool isGameRunning = SDL_TRUE;
+InputState gameInputState;
+vector<Sprite> spritesAssets;
+vector<Text> textAssets;
+vector<Bgm> musicAssets;
+
+///////// Variables y Constantes Globales /////////////
+
+///////// Funciones de inicializacion y destruccion /////////////
+void initEngine()
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024);
+
+    TTF_Init();
+
+    int initted = Mix_Init(MIX_INIT_MP3);
+    if ( (initted & MIX_INIT_MP3) != MIX_INIT_MP3) {
+        cout << "Mix_Init: Failed to init required ogg and mod support!" << endl;
+        cout << "Mix_Init: " << Mix_GetError() << endl;
+        // handle error
+    }
+
     window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
 
+void destroyEngine() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    TTF_Quit();
+    Mix_CloseAudio();
+    Mix_Quit();
+}
+
+///////// Funciones de inicializacion y destruccion /////////////
+
+
+///////// Funciones de carga y liberacion de recursos /////////////
+
+void loadAssets() {
+
+    // Cargo el Logo principal.
     string filePath = "assets/img/logo.png";
     SDL_Texture* texture = IMG_LoadTexture(renderer, filePath.c_str());
-
     SDL_Rect dest;
-    dest.x = 0;
+    dest.x = WIDTH >> 2;
     dest.y = 0;
-    dest.w = WIDTH;
-    dest.h = HEIGHT;
-   
-    SDL_bool done = SDL_FALSE;
+    dest.w = WIDTH >> 1;
+    dest.h = HEIGHT >> 1;
+
+    Sprite logoSprite;
+    logoSprite.dest = dest;
+    logoSprite.texture = texture;
+    spritesAssets.push_back(logoSprite);
+
+    // Cargo el texto...
     string fontfilePath = "assets/fonts/arial.ttf";
-    TTF_Init();
+
     TTF_Font* Sans = TTF_OpenFont(fontfilePath.c_str(), 24); //this opens a font style and sets a size
-   
+
     SDL_Color White = { 255, 255, 255 };  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
 
     SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "Project ready...", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
@@ -41,45 +115,153 @@ int main(int argc, char* argv[])
     SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); //now you can convert it into a texture
 
     SDL_Rect Message_rect; //create a rect
-    Message_rect.w = WIDTH * 0.85; // controls the width of the rect
-    Message_rect.h = HEIGHT * 0.20; // controls the height of the rect
+    Message_rect.w = WIDTH * 0.65; // controls the width of the rect
+    Message_rect.h = HEIGHT * 0.10; // controls the height of the rect
     Message_rect.x = (WIDTH >> 1) - (Message_rect.w >> 1);  //controls the rect's x coordinate 
     Message_rect.y = HEIGHT >> 1; // controls the rect's y coordinte
-   
 
-    while (!done) {
-        SDL_Event event;
+    Text mainText;
+    mainText.font = Sans;
+    mainText.color = White;
+    mainText.surface = surfaceMessage;
+    mainText.texture = Message;
+    mainText.dest = Message_rect;
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &dest);
+    textAssets.push_back(mainText);
 
-        //Mind you that (0,0) is on the top left of the window/screen, think a rect as the text's box, that way it would be very simple to understand
 
-        //Now since it's a texture, you have to put RenderCopy in your game loop area, the area where the whole code executes
+    // Cargo Sonidos y BGM
+    string soundFilePath = "assets/bgm/littleidea.mp3";
+    Mix_Music* music;
+    music = Mix_LoadMUS(soundFilePath.c_str());
+    
+    Bgm bgm01;
+    bgm01.music = music;
 
-        SDL_RenderCopy(renderer, Message, NULL, &Message_rect); //you put the renderer's name first, the Message, the crop size(you can ignore this if you don't want to dabble with cropping), and the rect which is the size and coordinate of your texture
+    musicAssets.push_back(bgm01);
 
-        SDL_RenderPresent(renderer);
+}
 
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                done = SDL_TRUE;
-            }
+void unloadAssets() {
+    for (int i = 0; i < spritesAssets.size(); i++) {
+        SDL_DestroyTexture(spritesAssets[i].texture);
+    }
+
+    for (int i = 0; i < textAssets.size(); i++) {
+        SDL_FreeSurface(textAssets[i].surface);
+        SDL_DestroyTexture(textAssets[i].texture);
+    }
+
+    for (int i = 0; i < musicAssets.size(); i++) {
+        Mix_FreeMusic(musicAssets[i].music);
+    }
+}
+
+///////// Funciones de carga y liberacion de recursos /////////////
+
+///////// Funciones de actualizacion y pintado /////////////
+
+void inputUpdate() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        
+        switch (event.type) {
+        case SDL_KEYDOWN:
+            break;
+        case SDL_KEYUP:
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            break;
+        case SDL_MOUSEBUTTONUP:
+            break;
+        case SDL_QUIT:
+            isGameRunning = SDL_FALSE;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+// Para ser usado en distintos contadores..
+float timer = 1.0f * 1000; // 1000 ms
+
+void updateGame(float deltaTime) {
+    const float BLINK_SPEED = 5.0f;
+
+    timer -= BLINK_SPEED * deltaTime;
+
+    // para efecto de parpadeo...
+    if (timer <= 0.0f) {
+        timer = 1.0f * 1000;
+        textAssets[0].isVisible = !textAssets[0].isVisible;
+    }
+}
+
+void render()
+{
+    // Limpio la pantalla 
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    // Pinto todos los sprites...
+    for (int i = 0; i < spritesAssets.size(); i++) {
+        if (spritesAssets[i].isVisible) {
+            SDL_RenderCopy(renderer, spritesAssets[i].texture, NULL, &spritesAssets[i].dest);
         }
     }
 
-    //Don't forget to free your surface and texture
-    SDL_FreeSurface(surfaceMessage);
-    SDL_DestroyTexture(Message);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    TTF_Quit();
+    // Pinto todos los textos...
+    for (int i = 0; i < textAssets.size(); i++) {
+        if (textAssets[i].isVisible) {
+            SDL_RenderCopy(renderer, textAssets[i].texture, NULL, &textAssets[i].dest);
+        }
+    }
+
+    // Presento la imagen en pantalla
+    SDL_RenderPresent(renderer);
+}
+
+///////// Funciones de actualizacion y pintado /////////////
+
+///////// Funcione principal y GameLoop /////////////
+
+int main(int argc, char* argv[])
+{
+    initEngine();
+
+    // Cargo Assets
+    loadAssets();
+
+    Mix_PlayMusic(musicAssets[0].music, -1);
+
+    int currentTime = SDL_GetTicks64();
+
+    while (isGameRunning) {
+
+        int previousTime = currentTime;
+
+        currentTime = SDL_GetTicks64();
+
+        float deltaTime = currentTime - previousTime;
+
+        inputUpdate();
+
+        updateGame(deltaTime);
+
+        render();
+    }
+
+    // Detendo la musica 
+    Mix_HaltMusic();
+
+    // Descargo Assets
+    unloadAssets(); 
+    destroyEngine();
     return 0;
 }
 
+///////// Funcione principal y GameLoop /////////////
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
